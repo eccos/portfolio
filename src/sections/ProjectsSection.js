d5dc7ff2193aedb55dcaa9a6e38bb3fc6c36fbbf
eccos <eccos@users.noreply.github.com>
2023-12-24 01:58:02 -0800

    WIP add projects in clickable Card format

--- src/sections/ProjectsSection.js
+++ src/sections/ProjectsSection.js
@@ -1,3 +1,38 @@
-export default function ProjectsSection() {
-  return 'Projects Section '.repeat(100);
+import {
+  Typography,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+} from '@mui/material';
+import { PROJECTS } from '../app/shared/PROJECTS';
+
+export default function ProjectsSection({ label }) {
+  const projects = PROJECTS;
+
+  return (
+    <>
+      <Typography variant="h4">{label}</Typography>
+      {projects.map((project) => (
+        <Card key={project.id} sx={{ maxWidth: 345 }}>
+          <CardActionArea href={project.githubUrl}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={project.img}
+              alt={project.title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {project.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {project.descShort}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </>
+  );
 }
