977b06a70f7cea8c44c832af8a476b75b580218d
eccos <eccos@users.noreply.github.com>
2023-12-24 15:39:35 -0800

    upd title color

--- src/sections/ProjectsSection.js
+++ src/sections/ProjectsSection.js
@@ -3,14 +3,16 @@ import {
   CardActionArea,
   CardContent,
   CardMedia,
-  Typography,
   Grid,
+  Typography,
+  useTheme,
 } from '@mui/material';
 import { PROJECTS } from '../app/shared/PROJECTS';
 import SectionHeader from '../components/SectionHeader';
 
 export default function ProjectsSection({ label }) {
   const projects = PROJECTS;
+  const theme = useTheme();
 
   return (
     <>
@@ -18,7 +20,7 @@ export default function ProjectsSection({ label }) {
       <Grid container spacing={2}>
         {projects.map((project) => (
           <Grid item key={project.id}>
-            <Card sx={{ maxWidth: 335 }} variant="outlined">
+            <Card sx={{ maxWidth: 300 }} variant="outlined">
               <CardActionArea href={project.githubUrl}>
                 <CardMedia
                   component="img"
@@ -27,7 +29,14 @@ export default function ProjectsSection({ label }) {
                   alt={project.title}
                 />
                 <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                    color={theme.palette.secondary.main}
+                    borderBottom={1}
+                    borderColor={theme.palette.primary.main}
+                  >
                     {project.title}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
