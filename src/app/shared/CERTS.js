b3009d79308c03ccbae6ad478dba9bd63f737ff9
eccos <eccos@users.noreply.github.com>
2023-12-23 16:08:24 -0800

    add certificate data

---
+++ src/app/shared/CERTS.js
@@ -0,0 +1,39 @@
+import FrontEndWebMobileImg from '../assets/img/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-Front_End_Web_and_Mobile_Development_Certificate_22829.jpg';
+import FrontEndReactNativeImg from '../assets/img/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-React_Native_Course_Certificate_22830.jpg';
+import DynamicWithReactImg from '../assets/img/certs/Dynamic_with_React-React_Course_Certificate_22760.jpg';
+import FrontEndJsFundamentalsImg from '../assets/img/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-JavaScript_Fundamentals_Course_Certificate_22005.jpg';
+
+import FrontEndWebMobilePdf from '../assets/pdf/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-Front_End_Web_and_Mobile_Development_Certificate_22829.pdf';
+import FrontEndReactNativePdf from '../assets/pdf/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-React_Native_Course_Certificate_22830.pdf';
+import DynamicWithReactPdf from '../assets/pdf/certs/Dynamic_with_React-React_Course_Certificate_22760.pdf';
+import FrontEndJsFundamentalsPdf from '../assets/pdf/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-JavaScript_Fundamentals_Course_Certificate_22005.pdf';
+
+export const CERTS = [
+  {
+    id: 'Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-Front_End_Web_and_Mobile_Development_Certificate',
+    title:
+      'Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-Front_End_Web_and_Mobile_Development_Certificate',
+    img: FrontEndWebMobileImg,
+    pdf: FrontEndWebMobilePdf,
+  },
+  {
+    id: 'Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-React_Native_Course_Certificate',
+    title:
+      'Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-React_Native_Course_Certificate',
+    img: FrontEndReactNativeImg,
+    pdf: FrontEndReactNativePdf,
+  },
+  {
+    id: 'Dynamic_with_React-React_Course_Certificate',
+    title: 'Dynamic_with_React-React_Course_Certificate',
+    img: DynamicWithReactImg,
+    pdf: DynamicWithReactPdf,
+  },
+  {
+    id: 'Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-JavaScript_Fundamentals_Course_Certificate',
+    title:
+      'Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-JavaScript_Fundamentals_Course_Certificate',
+    img: FrontEndJsFundamentalsImg,
+    pdf: FrontEndJsFundamentalsPdf,
+  },
+];
