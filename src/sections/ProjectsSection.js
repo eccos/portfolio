ffb02b5ce1245aea0ae5c6a1d19751e486e56ef3
eccos <eccos@users.noreply.github.com>
2023-12-24 03:45:28 -0800

    wrap Cards in Grid

--- src/sections/ProjectsSection.js
+++ src/sections/ProjectsSection.js
@@ -4,6 +4,7 @@ import {
   CardContent,
   CardMedia,
   Typography,
+  Grid,
 } from '@mui/material';
 import { PROJECTS } from '../app/shared/PROJECTS';
 import SectionHeader from '../components/SectionHeader';
@@ -14,30 +15,30 @@ export default function ProjectsSection({ label }) {
   return (
     <>
       <SectionHeader>{label}</SectionHeader>
-      {projects.map((project) => (
-        <Card
-          key={project.id}
-          sx={{ maxWidth: 345, marginBottom: 1 }}
-          variant="outlined"
-        >
-          <CardActionArea href={project.githubUrl}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={project.img}
-              alt={project.title}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {project.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {project.descShort}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
+      <Grid container spacing={2}>
+        {projects.map((project) => (
+          <Grid item key={project.id}>
+            <Card sx={{ maxWidth: 335 }} variant="outlined">
+              <CardActionArea href={project.githubUrl}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={project.img}
+                  alt={project.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {project.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {project.descShort}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
