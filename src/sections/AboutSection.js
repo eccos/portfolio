e8be56f03869dce0135c97f53f9739092f2990a0
eccos <eccos@users.noreply.github.com>
2023-12-24 03:26:15 -0800

    minor style change (color/border)

--- src/sections/AboutSection.js
+++ src/sections/AboutSection.js
@@ -1,10 +1,18 @@
-import { Link, Stack, Typography } from '@mui/material';
+import { Link, Stack, Typography, useTheme } from '@mui/material';
 
 export default function AboutSection() {
+  const theme = useTheme();
+
   return (
     <>
       <Stack direction={'row'} spacing={2}>
-        <Typography variant="h3" flex={1}>
+        <Typography
+          variant="h3"
+          flex={1}
+          borderBottom={1}
+          borderColor={theme.palette.secondary.main}
+          color={theme.palette.primary.main}
+        >
           Luis Delamora
         </Typography>
         <Typography variant="subtitle2" alignSelf={'end'}>
@@ -12,7 +20,9 @@ export default function AboutSection() {
           <Link href="https://github.com/eccos">GitHub</Link>
         </Typography>
       </Stack>
-      <Typography variant="overline">Full Stack Web Developer</Typography>
+      <Typography variant="overline" color={theme.palette.primary.main}>
+        Full Stack Web Developer
+      </Typography>
       <Typography variant="body1">
         Over 5 years of experience in front-end & back-end technologies creating
         responsive, accessible, intuitive, and frustration-free websites with
