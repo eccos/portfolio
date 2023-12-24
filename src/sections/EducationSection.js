548fa317215c23d55b6872c0e5d2b5044bc3a777
eccos <eccos@users.noreply.github.com>
2023-12-23 17:30:40 -0800

    add cert imgs & links to pdfs

--- src/sections/EducationSection.js
+++ src/sections/EducationSection.js
@@ -1,25 +1,24 @@
-// import { ImageList, ImageListItem } from '@mui/material';
+import { Link, Stack, Typography } from '@mui/material';
+import { CERTS } from '../app/shared/CERTS';
 
-export default function EducationSection() {
-  // <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-  //   {itemData.map((item) => (
-  //     <ImageListItem key={item.img}>
-  //       <img
-  //         srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-  //         src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-  //         alt={item.title}
-  //         loading="lazy"
-  //       />
-  //     </ImageListItem>
-  //   ))}
-  // </ImageList>;
+export default function EducationSection({ label }) {
+  const certs = CERTS;
 
-  return `CERTIFICATE IN FULL STACK WEB & MOBILE DEVELOPMENT, 2024, nucamp CODING BOOTCAMP, BELLevue, WA
-Front-End Web & Mobile Dev: React Native 	(Certification Dec 2023) 
-Front-End Web Development: React 		(Certification Nov 2023) 
-JavaScript Fundamentals 			(Certification Oct 2023)
-Associate in applied science, 2012, Columbia Basin College, Pasco, WA
-Computer Science – Programming and Software Development
-Certifications: VB.NET & C++ Development
-`;
+  return (
+    <>
+      <Typography variant="h4">{label}</Typography>
+      <Stack direction="row" spacing={2} overflow={'auto'}>
+        {certs.map((cert) => (
+          <Link key={cert.img} href={cert.pdf} target="_blank">
+            <img
+              src={cert.img}
+              alt={cert.title}
+              loading="lazy"
+              style={{ width: 200 }}
+            />
+          </Link>
+        ))}
+      </Stack>
+    </>
+  );
 }
