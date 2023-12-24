b1ad56056ba2848f7ada9e8c118ac103e0fdfbd0
eccos <eccos@users.noreply.github.com>
2023-12-24 01:54:59 -0800

    minor text edit

--- src/sections/AboutSection.js
+++ src/sections/AboutSection.js
@@ -9,15 +9,14 @@ export default function AboutSection() {
         </Typography>
         <Typography variant="subtitle2" alignSelf={'end'}>
           <Link href="https://linkedin.com/in/luis-delamora">LinkedIn</Link> |{' '}
-          <Link href="https://github.com/eccos">GitHub</Link> |{' '}
-          <Link href="https://codepen.io/eccos">CodePen</Link>
+          <Link href="https://github.com/eccos">GitHub</Link>
         </Typography>
       </Stack>
-      <Typography variant="overline">Full-Stack Web Developer</Typography>
+      <Typography variant="overline">Full Stack Web Developer</Typography>
       <Typography variant="body1">
-        Over 5 years of experience in front-end & back-end technologies,
-        creating responsive, accessible, intuitive, and frustration-free
-        websites with stylish flair.
+        Over 5 years of experience in front-end & back-end technologies creating
+        responsive, accessible, intuitive, and frustration-free websites with
+        stylish designs.
       </Typography>
       <Typography variant="body2">
         Constantly self-improving by absorbing the latest software technology
