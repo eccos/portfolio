0a9fc01f6f5522750f1da5827dc3c3d37bef1b9c
eccos <eccos@users.noreply.github.com>
2023-12-27 20:46:00 -0800

    add transparency

--- src/FixedTopNav.js
+++ src/FixedTopNav.js
@@ -1,18 +1,26 @@
 import { useState } from 'react';
 
 import { AppBar, Tab, Tabs, Toolbar } from '@mui/material';
+import { alpha } from '@mui/material/styles';
 
 function FixedTopNav({ sections }) {
   const [value, setValue] = useState(0);
 
   return (
-    <AppBar id="top-nav-container" position="sticky" sx={{ top: 0 }}>
+    <AppBar
+      id="top-nav-container"
+      position="sticky"
+      sx={{
+        top: 0,
+        backgroundColor: (theme) => alpha(theme.palette.primary.main, 0.8),
+      }}
+    >
       <Toolbar sx={{ justifyContent: 'center' }}>
         <Tabs
           value={value}
           onChange={(_, val) => setValue(val)}
           centered
-          textColor={'white'}
+          textColor={'inherit'}
           indicatorColor={'secondary'}
         >
           {sections.map((section) => (
