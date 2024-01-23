fcc325a2464fcc3339cd7522e07757b96198d321
eccos <eccos@users.noreply.github.com>
2024-01-23 01:35:34 -0800

    add new nucamp certs

--- src/app/shared/CERTS.js
+++ src/app/shared/CERTS.js
@@ -1,14 +1,30 @@
+import FullStackWebMobileImg from '../assets/img/certs/Backend_with_NodeJS-Full_Stack_Web_and_Mobile_Development_Certificate_23050.jpg';
+import FullStackWebMobilePdf from '../assets/pdf/certs/Backend_with_NodeJS-Full_Stack_Web_and_Mobile_Development_Certificate_23050.pdf';
+import BackEndNodeExpressImg from '../assets/img/certs/Backend_with_NodeJS-NodeJS_Express_and_MongoDB_Course_Certificate_23049.jpg';
+import BackEndNodeExpressPdf from '../assets/pdf/certs/Backend_with_NodeJS-NodeJS_Express_and_MongoDB_Course_Certificate_23049.pdf';
 import FrontEndWebMobileImg from '../assets/img/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-Front_End_Web_and_Mobile_Development_Certificate_22829.jpg';
-import FrontEndReactNativeImg from '../assets/img/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-React_Native_Course_Certificate_22830.jpg';
-import DynamicWithReactImg from '../assets/img/certs/Dynamic_with_React-React_Course_Certificate_22760.jpg';
-import FrontEndJsFundamentalsImg from '../assets/img/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-JavaScript_Fundamentals_Course_Certificate_22005.jpg';
-
 import FrontEndWebMobilePdf from '../assets/pdf/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-Front_End_Web_and_Mobile_Development_Certificate_22829.pdf';
+import FrontEndReactNativeImg from '../assets/img/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-React_Native_Course_Certificate_22830.jpg';
 import FrontEndReactNativePdf from '../assets/pdf/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-React_Native_Course_Certificate_22830.pdf';
+import DynamicWithReactImg from '../assets/img/certs/Dynamic_with_React-React_Course_Certificate_22760.jpg';
 import DynamicWithReactPdf from '../assets/pdf/certs/Dynamic_with_React-React_Course_Certificate_22760.pdf';
+import FrontEndJsFundamentalsImg from '../assets/img/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-JavaScript_Fundamentals_Course_Certificate_22005.jpg';
 import FrontEndJsFundamentalsPdf from '../assets/pdf/certs/Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-JavaScript_Fundamentals_Course_Certificate_22005.pdf';
 
 export const CERTS = [
+  {
+    id: 'Backend_with_NodeJS-Full_Stack_Web_and_Mobile_Development_Certificate',
+    title:
+      'Backend_with_NodeJS-Full_Stack_Web_and_Mobile_Development_Certificate',
+    img: FullStackWebMobileImg,
+    pdf: FullStackWebMobilePdf,
+  },
+  {
+    id: 'Backend_with_NodeJS-NodeJS_Express_and_MongoDB_Course_Certificate',
+    title: 'Backend_with_NodeJS-NodeJS_Express_and_MongoDB_Course_Certificate',
+    img: BackEndNodeExpressImg,
+    pdf: BackEndNodeExpressPdf,
+  },
   {
     id: 'Front_End_Web_UI_Frameworks_and_Tools_Bootstrap_4-Front_End_Web_and_Mobile_Development_Certificate',
     title:
